5f2d759ab0ac8fa9e455edbe1759f7f94f6b060a
Aditya <S3822249@student.rmit.edu.au>
2022-06-03 22:20:00 +1000

    Initial Commit

--- src/Food.java
+++ src/Food.java
@@ -1,6 +1,53 @@
 public class Food {
 
+    private String foodID;
 
-    int i =1;
+    private String foodName;
+
+    private String foodDescription;
+
+    private double foodPrice;
+
+    private int foodCalorie;
+
+    private String foodType;
+
+    public Food (String foodID,String foodName, String foodDescription, double foodPrice, int foodCalorie, String foodType)
+
+    {
+        this.foodCalorie=foodCalorie;
+        this.foodID=foodID;
+        this.foodName=foodName;
+        this.foodDescription=foodDescription;
+        this.foodPrice=foodPrice;
+        this.foodType=foodType;
+
+    }
+
+    public boolean AddFood(String foodID,String foodName, String foodDescription, double foodPrice, int foodCalorie, String foodType)
+
+    {
+
+        // add the information of food to a TXT file
+
+        // if the food information is added to the TXT file, it should return true;
+
+        // if the food information cannot be added to the TXT file, it should return false;
+        return false;
+
+    }
+
+    public boolean UpdateFood(String foodID,String foodName, String foodDescription, double foodPrice, int foodCalorie, String foodType)
+
+    {
+
+        //  Update the information of a given food in a TXT file
+
+        // if the food information is updated in the TXT file, it should return true;
+
+        // if the food information cannot be updated in the TXT file, it should
+        return false;
+
+    }
 
 }
